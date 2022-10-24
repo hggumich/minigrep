b0a2587cccff1692198faf3240468f3495ef0a4f
Hector Garza <hggumich@gmail.com>
2022-10-24 16:59:54 -0400

    add text file and readin function

--- Desktop/projects/minigrep/src/main.rs
+++ Desktop/projects/minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,4 +11,8 @@ fn main() {
     println!("Searching for {}", query);
     println!("In file {}", file_path);
     println!("What is stored at zero {}", zero);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
