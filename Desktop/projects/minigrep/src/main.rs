58b0b6d3abc017bb892af20c70da83ef22190b7b
Hector Garza <hggumich@gmail.com>
2022-10-24 17:21:01 -0400

    extract the argument parser

--- Desktop/projects/minigrep/src/main.rs
+++ Desktop/projects/minigrep/src/main.rs
@@ -5,9 +5,9 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let zero = &args[0];
-    let query = &args[1];
-    let file_path = &args[2];
     
+    let (query, file_path) = parse_config(&args);
+
     println!("Searching for {}", query);
     println!("In file {}", file_path);
     println!("What is stored at zero {}", zero);
@@ -15,4 +15,12 @@ fn main() {
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    
+    let query = &args[1];
+    let file_path = &args[2];
+
+    (query, file_path)
 }
